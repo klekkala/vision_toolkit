c1dc6b5b9171bc46bc656e9a30da53623519ff08
slavazin <slavazinevich@gmail.com>
2021-05-29 14:27:59 -0700

    Added classes for import and for parsing

--- src/pano/pano.cpp
+++ src/pano/pano.cpp
@@ -58,7 +58,109 @@ int main(int argc, const char *argv[]) {
 
    cv::Stitcher::Status status = stitcher->stitch(imgs, pano);
 
-   cv::imwrite(out_str, pano);
+
 
    return 0;
+}
+
+class CliParser
+{
+public:
+    CliParser(int argc, const char * const * argv);
+    void AddArgument(string arg_names);
+    void AddArgument(string arg_names, string explanation);
+    void Parse();
+    string& GetArgument(string arg);
+
+private:
+    bool _hasParsed = false;
+    std::map<string, string&> _arg_map;
+    options_description _desc;
+    int _argc;
+    const char * const * _argv;
+};
+
+CliParser::CliParser(int argc, const char * const * argv) : _argc(argc), _argv(argv)
+{
+}
+void CliParser::AddArgument(string arg_names, string explanation)
+{
+    string in;
+    _desc.add_options()
+            (arg_names.c_str(), value<std::string>(&in),
+             explanation.c_str());
+    stringstream ss(arg_names);
+    string word;
+    while(getline(ss, word, ','))
+    {
+        _arg_map.insert(pair<string, string&>(word, in));
+    }
+}
+
+void CliParser::Parse() {
+    if(_hasParsed) return;
+    variables_map vm;
+    auto desc = _desc;
+    store(parse_command_line(_argc, _argv, _desc), vm);
+    notify(vm);
+    _hasParsed = true;
+}
+
+string & CliParser::GetArgument(string arg)
+{
+    auto search_result = _arg_map.find(arg);
+    if(search_result == _arg_map.end())
+    {
+        cout << "could not find argument!";
+        return (string &) "";
+    }
+    return _arg_map.at(arg);
+}
+
+
+class ImageIO
+{
+public:
+    std::vector<cv::Mat>& Read(string in_str);
+    void Write(const cv::Mat& img, string out_path);
+};
+
+std::vector<cv::Mat> & ImageIO::Read(string in_str)
+{
+    std::vector<cv::Mat> imgs;
+    path in_path = path(in_path);
+    boost::filesystem::directory_iterator it{in_path};
+
+    while (it != boost::filesystem::directory_iterator{}) {
+        cv::Mat img = cv::imread((it->path()).string(), cv::IMREAD_COLOR);
+        imgs.emplace_back(img);
+        ++it;
+    }
+    return imgs;
+}
+
+void ImageIO::Write(const cv::Mat &img, string out_path)
+{
+    cv::imwrite(out_path, img);
+}
+
+class ImageImporter
+{
+public:
+    ImageImporter(CliParser& parser);
+    cv::Mat ImportImage();
+private:
+    CliParser& _parser;
+    ImageIO _imageIO;
+};
+ImageImporter::ImageImporter(CliParser &parser) : _parser(parser)
+{
+    parser.AddArgument("input,i", "Input location for input images");
+    parser.AddArgument("output,o","Output location for stitched panorama");
+}
+
+cv::Mat ImageImporter::ImportImage()
+{
+    _parser.Parse();
+    _imageIO.Read(_parser.GetArgument("i"));
 }
