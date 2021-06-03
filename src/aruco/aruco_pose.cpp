1c6b698e07c9491161d0de6d584f42df81aeec36
Mihai Bibireata <mihai.bibireata@gmail.com>
2021-06-03 12:45:10 -0700

    more cmake changes, ROS seems to be on OpenCV 3.X, may be problematic later

--- src/aruco/aruco_pose.cpp
+++ src/aruco/aruco_pose.cpp
@@ -6,9 +6,12 @@
  * \date May 28 2021
  */
 
+#include <iostream>
+
 #include <opencv2/aruco.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+//#include <opencv2/opencv.hpp>
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 
@@ -18,6 +21,55 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 
-int main() {
-   return 0;
+#include "common/CliParser.h"
+
+using namespace cv;
+using namespace std;
+
+//int main(int argc, const char * argv[]) {
+//   return 0;
+//}
+//
+int main(int argc, const char *argv[])
+{
+   CliParser parser(argc, argv);
+   parser.AddArgument("input,i", "Input mp4 file");
+   parser.Parse();
+
+   VideoCapture cap(parser.GetArgument("i"));
+   cout << parser.GetArgument("i") << endl;
+
+  // Check if camera opened successfully
+  if(!cap.isOpened()){
+    cout << "Error opening video stream or file" << endl;
+    return -1;
+  }
+	
+  while(1){
+
+    Mat frame;
+    // Capture frame-by-frame
+    cap >> frame;
+ 
+    // If the frame is empty, break immediately
+    if (frame.empty())
+      break;
+
+    // Display the resulting frame
+    imshow( "Frame", frame );
+
+    // Press  ESC on keyboard to exit
+    char c=(char)waitKey(25);
+    if(c==27)
+      break;
+  }
+ 
+  // When everything done, release the video capture object
+  cap.release();
+
+  // Closes all the frames
+  destroyAllWindows();
+	
+  return 0;
 }
+// END OF PROGRAM
