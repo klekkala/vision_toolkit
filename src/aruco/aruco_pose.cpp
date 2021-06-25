393191a09a00c42d7bbc8ad5a4a52a2e7656aea3
mbibireata <mihai.bibireata@gmail.com>
2021-06-25 12:02:25 -0700

    offline interpolation working?

--- src/aruco/aruco_pose.cpp
+++ src/aruco/aruco_pose.cpp
@@ -39,7 +39,7 @@ typedef std::vector<std::vector<cv::Point2f>> opencv_aruco_2d_pts_t;
 
 typedef struct MarkerData {
   std::vector<cv::Point2f> corners;
-  cv::Vec3d rvecs, tvecs;
+  cv::Vec3d rvec, tvec;
 } MarkerData;
 
 typedef struct ObsData {
@@ -47,7 +47,7 @@ typedef struct ObsData {
   std::vector<opencv_aruco_2d_pts_t> rejected_candidates;
 } ObsData;
 
-//typedef std::unordered_map<int32_t, size_t> marker_id_to_frame_id_t;
+typedef std::unordered_map<int32_t, std::vector<size_t>> marker_to_frame_t;
 typedef std::unordered_map<size_t, struct ObsData*> frame_to_obs_t;
 
 /** Detects aruco markers
@@ -64,6 +64,7 @@ void DetectAruco(const Bag& bag,
                  const cv::Ptr<cv::aruco::DetectorParameters>& parameters,
                  const cv::Ptr<cv::aruco::Dictionary>& dictionary,
                  frame_to_obs_t& frame_to_obs,
+                 marker_to_frame_t& marker_to_frame,
                  cv::Mat& cam_mat,
                  cv::Mat& dst_mat,
                  std::vector<cv::Mat>& imgs) {
@@ -124,9 +125,15 @@ void DetectAruco(const Bag& bag,
     for (int i = 0; i < rvecs.size(); ++i) {
       struct MarkerData* marker_data = new MarkerData();
       marker_data->corners = corners[i];
-      marker_data->tvecs = tvecs[i];
-      marker_data->rvecs = rvecs[i];
+      marker_data->tvec = tvecs[i];
+      marker_data->rvec = rvecs[i];
       obs_data->id_to_data.emplace(ids[i], marker_data);
+
+      if (marker_to_frame.find(ids[i]) != marker_to_frame.end()) {
+        marker_to_frame.at(ids[i]).emplace_back(frame_id);
+      } else {
+        marker_to_frame.emplace(ids[i], std::vector<size_t>({ frame_id }));
+      }
     } 
     obs_data->rejected_candidates.emplace_back(rejected);
     frame_to_obs.emplace(frame_id, obs_data);
@@ -155,6 +162,56 @@ cv::Rect RectBBOXFromPts(const std::vector<cv::Point2f>& pts) {
     br_y = max(pt.y, br_y);
   }
   return cv::Rect(tl_x, tl_y, br_x - tl_x, br_y - tl_y);
+}
+
+void InterpolateMarkers(const frame_to_obs_t& frame_to_obs,
+                        const marker_to_frame_t& marker_to_frame,
+                        const cv::Mat& cam_mat,
+                        const cv::Mat& dst_mat,
+                        std::vector<cv::Mat>& imgs) {
+  for (const auto& marker_and_frame : marker_to_frame) {
+    auto& marker_id = marker_and_frame.first;
+    auto& frames = marker_and_frame.second;
+
+    size_t gap = 0;
+    for (size_t i = 1; i < frames.size(); ++i) {
+      gap = frames[i] - frames[i - 1]; 
+      if (gap == 2) continue;
+      auto& t_init = frame_to_obs.at(frames[i - 1])->id_to_data.at(marker_id)->tvec;
+      auto& r_init = frame_to_obs.at(frames[i - 1])->id_to_data.at(marker_id)->rvec;
+      auto& t_final = frame_to_obs.at(frames[i])->id_to_data.at(marker_id)->tvec;
+      auto& r_final = frame_to_obs.at(frames[i])->id_to_data.at(marker_id)->rvec;
+
+      auto d_trans = t_final - t_init;
+      auto d_rot = r_final - r_init;
+      cv::divide(d_trans, gap, d_trans);
+      cv::divide(d_rot, gap, d_rot);
+
+      // Debug prints
+      /*
+      cout << "init frame: " << frames[i - 1] << ", final frame: " << frames[i] << endl;
+      cout << "t init:  " << t_init << endl;
+      cout << "r init:  " << r_init << endl;
+      cout << "t final: " << t_final << endl;
+      cout << "r final: " << r_final << endl;
+      cout << "d trans: " << d_trans << endl;
+      cout << "d rot:   " << d_rot << endl;
+      */
+
+      // Draw intermediate poses
+      for (size_t j = 1; j <= gap; ++j) {
+        auto target_frame = frames[i - 1] + j;
+        cv::Vec3d target_t, target_r;
+        cv::multiply(d_trans, j, target_t);
+        cv::multiply(d_rot, j, target_r);
+        target_t += t_init;
+        target_r += r_init;
+        //cv::aruco::drawAxis(imgs[target_frame], cam_mat, dst_mat, target_r, target_t, 0.1);
+        cv::aruco::drawAxis(imgs[target_frame], cam_mat, dst_mat, r_init, target_t, 0.1);
+        
+      }
+    }
+  }
 }
 
 int main(int argc, const char *argv[])
@@ -200,9 +257,14 @@ int main(int argc, const char *argv[])
   cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
   cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_ARUCO_ORIGINAL);
   frame_to_obs_t frame_to_obs;
+  marker_to_frame_t marker_to_frame;
   std::vector<cv::Mat> imgs;
 
-  DetectAruco(bag, parameters, dictionary, frame_to_obs, cam_mat, dst_mat, imgs);
+  DetectAruco(bag, parameters, dictionary, frame_to_obs, marker_to_frame, cam_mat, dst_mat, imgs);
+
+  cout << "Number of markers detected: " << marker_to_frame.size() << endl;
+
+  InterpolateMarkers(frame_to_obs, marker_to_frame, cam_mat, dst_mat, imgs);
   
   if (imgs.empty()) {
     return EXIT_FAILURE;
