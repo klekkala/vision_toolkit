222b9ef68daac842735e6a2c7a86d4b65bb51a49
Henghui <henghuib@usc.edu>
2025-01-16 14:14:39 -0800

    chore: Get bags from data folder instead of downloading them

--- extract/bag_.sh
+++ extract/bag_.sh
@@ -5,13 +5,11 @@ fi
 
 for ((i = 1; i<= 5; i++))
 do
-  index=0
-  for entry in $(ls -1v /lab/tmpig23b/navisim/data/bags/$1/$index/cam$i/*.bag); do
+  for entry in $(ls -1v /data/$1/cam$i/*.bag); do
       if [ "${entry: -4}" != ".bag" ]; then
           continue
       fi
       echo "${entry}"
-      python3 ./vision_toolkit/extract/read_bag_.py --bag "${entry}" --cam "$i" --save $1 --num $index
-      ((index++))
+      python3 ./vision_toolkit/extract/read_bag_.py --bag "${entry}" --cam "$i" --date $1
   done
 done
