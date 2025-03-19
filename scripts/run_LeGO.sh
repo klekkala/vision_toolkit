149f8ea70896867a78b7ffb92d2f51e92726708e
Henghui <henghuib@usc.edu>
2025-03-18 18:20:43 -0700

    chore: Add logs to scripts progress

--- scripts/run_LeGO.sh
+++ scripts/run_LeGO.sh
@@ -7,35 +7,37 @@ fi
 script_dir=$(dirname "$(realpath "$0")")
 date=$(basename "$1")
 
-bag_count=0
-LEGO_FOLDER="/lab/tmpig23b/vision_toolkit/data/bag_dump/$1/$2/all_lego"
-ODOM_FOLDER="/lab/tmpig23b/vision_toolkit/data/bag_dump/$1/$2/all_odom"
-
-if [ ! -d "$LEGO_FOLDER" ]; then
-    echo "Folder '$LEGO_FOLDER' does not exist. Creating it now."
-    mkdir -p "$LEGO_FOLDER"
-fi
-
-if [ ! -d "$ODOM_FOLDER" ]; then
-    echo "Folder '$ODOM_FOLDER' does not exist. Creating it now."
-    mkdir -p "$ODOM_FOLDER"
-fi
+session=0
+OUTPUT_DIR="/lab/tmpig23b/vision_toolkit/data/bag_dump/$1"
 
 # Loop through each bag file and execute the script
-for bag_file in $(ls -v /lab/tmpig23b/vision_toolkit/data/blocks/$1/$2/cam1/*.bag); do
+for bag_file in $(ls -v /data/$1/cam1/*.bag); do
     $script_dir/auto_LeGO.sh "$bag_file"
+    
     sleep 30
     if [ ! -s /tmp/odometry.txt ]; then
+        echo "Error while processing $date/$session" 
         echo "$1 $bag_file" >> LeGO_error.txt
     else
-        mv /tmp/odometry.txt "$ODOM_FOLDER/odometry_$bag_count.txt"
-        mv /tmp/cornerMap.pcd "$LEGO_FOLDER/cornerMap_$bag_count.pcd"
-        mv /tmp/trajectory.pcd "$LEGO_FOLDER/trajectory_$bag_count.pcd"
-        mv /tmp/surfaceMap.pcd "$LEGO_FOLDER/surfaceMap_$bag_count.pcd"
+        echo "Success! Moving files to $OUTPUT_DIR/$session"
+        if [ ! -d "$OUTPUT_DIR/$session/all_odom" ]; then
+            echo "Folder '$OUTPUT_DIR/$session/all_odom' does not exist. Creating it now."
+            mkdir -p "$OUTPUT_DIR/$session/all_odom"
+        fi
+
+        if [ ! -d "$OUTPUT_DIR/$session/all_lego" ]; then
+            echo "Folder '$OUTPUT_DIR/$session/all_lego' does not exist. Creating it now."
+            mkdir -p "$OUTPUT_DIR/$session/all_lego"
+        fi
+
+        mv /tmp/odometry.txt "$OUTPUT_DIR/$session/all_odom/odometry.txt"
+        mv /tmp/cornerMap.pcd "$OUTPUT_DIR/$session/all_lego/cornerMap.pcd"
+        mv /tmp/trajectory.pcd "$OUTPUT_DIR/$session/all_lego/trajectory.pcd"
+        mv /tmp/surfaceMap.pcd "$OUTPUT_DIR/$session/all_lego/surfaceMap.pcd"
     fi
 
     sleep 20
-    ((bag_count+=1))
+    ((session++))
 done
 
 
