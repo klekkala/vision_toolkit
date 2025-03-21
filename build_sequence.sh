6868f8fe6d4f67eb9e8a4ed53c794583db68166c
Henghui <henghuib@usc.edu>
2025-03-20 21:24:52 -0700

    feat: Implement scripts for building sequence and update gitignore

--- build_sequence.sh
+++ build_sequence.sh
@@ -10,5 +10,6 @@ WINDOW_SIZE=20
 SRC_DIR='/lab/tmpig23b/vision_toolkit/data/bag_dump'
 SEQ_OUT='.'
 
-# ./scripts/sector2seq/sector2sequence.sh $DATE $SESSION $WINDOW_SIZE $SRC_DIR $SEQ_OUT
+./scripts/sector2seq/sector2sequence.sh $DATE $SESSION $WINDOW_SIZE $SRC_DIR $SEQ_OUT
 ./scripts/sequence_graph/run_compute_sequence_graph.sh $DATE $SESSION $OUT_DIR $OUT_DIR
+./scripts/elevation_map/plot_elevation_map.sh $DATE $SESSION $OUT_DIR 
