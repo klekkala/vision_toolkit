c066178593abdc1fb1c4fc65711e7d155f36fb12
Henghui <henghuib@usc.edu>
2025-03-20 23:28:25 -0700

    chore: Add for loop to handle multiple sequences

--- build_sequence.sh
+++ build_sequence.sh
@@ -8,8 +8,8 @@ DATE=$1
 SESSION=$2
 WINDOW_SIZE=20
 SRC_DIR='/lab/tmpig23b/vision_toolkit/data/bag_dump'
-SEQ_OUT='.'
+OUT_DIR='.'
 
-./scripts/sector2seq/sector2sequence.sh $DATE $SESSION $WINDOW_SIZE $SRC_DIR $SEQ_OUT
+./scripts/sector2seq/sector2sequence.sh $DATE $SESSION $WINDOW_SIZE $SRC_DIR $OUT_DIR
 ./scripts/sequence_graph/run_compute_sequence_graph.sh $DATE $SESSION $OUT_DIR $OUT_DIR
 ./scripts/elevation_map/plot_elevation_map.sh $DATE $SESSION $OUT_DIR 
