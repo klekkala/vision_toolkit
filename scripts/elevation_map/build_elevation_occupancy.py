ecc45cf1d062544c090bdf2432eb9754147185fa
Henghui <henghuib@usc.edu>
2025-03-20 21:27:55 -0700

    chore :  Update input arguments for occupancy map generation

--- scripts/elevation_map/build_elevation_occupancy.py
+++ scripts/elevation_map/build_elevation_occupancy.py
@@ -1,12 +1,16 @@
 import os
 import sys
 import open3d as o3d
+import argparse
+
+from pathlib import Path
+
 from interpolation import *
 from read_map import *
 
 
 def build_elevation_occupancy_map(pcd_path, output_path):
-    pcd = o3d.io.read_point_cloud(pcd_path)
+    pcd = o3d.io.read_point_cloud(pcd_path.as_posix())
     elevation_map = get_elevation_map(pcd)
     elevation_map = interpolation(elevation_map)
     np.save(os.path.join(output_path, 'elevation_map.npy'), elevation_map)
@@ -15,11 +19,14 @@ def build_elevation_occupancy_map(pcd_path, output_path):
     occupancy_map = (elevation_map >= occupancy_threshold).astype(int)
     np.save(os.path.join(output_path, 'occupancy_map.npy'), occupancy_map)
 
-
 if __name__ == "__main__":
-    pcd_path = sys.argv[1]     # Input sec5 surfaceMap.pcd
-    output_path = sys.argv[2]  # Output folder path resulting elevation Map, occupancy Map, and interpolation resul
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', type=str)
+    parser.add_argument('--date', type=str)
+    parser.add_argument('--session',type=str)
+    parser.add_argument('--sequence', type=str)
+    args = parser.parse_args()
 
-    build_elevation_occupancy_map(pcd_path, output_path)
-
-# python build_elevation_occupancy.py C:\Users\andyz\OneDrive\Desktop\Codes\Projects\1.usc-projects\ilab\navisim\navisim\assets\gaussian_splat_data\sec4\surfaceMap.pcd ./sec4/
+    path = Path(args.path, args.date, args.session, args.sequence)
+    pcd_path = Path(path, 'sequence.pcd')
+    build_elevation_occupancy_map(pcd_path, path)
